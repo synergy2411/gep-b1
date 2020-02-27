15cfcf75fe850f772ff71a4610403dc247c3d482
unknown <synergy2411@outlook.com>
2020-02-27 17:27:56 +0530

    image added

--- users-space/projects/users-app/src/app/components/users/users.component.ts
+++ users-space/projects/users-app/src/app/components/users/users.component.ts
@@ -2,10 +2,18 @@ import { Component } from "@angular/core";
 
 @Component({
   selector : "app-users",
-  template : `
-    <h1> Users Loaded Succsessfully!</h1>
-  `
+  templateUrl : `./users.component.html`
 })
 export class UserComponent{
-  
+
+  user = {
+    firstName : "Bill",
+    lastName : "Gates",
+    dob : new Date("Dec 21, 1965"),
+    isWorking : true,
+    income : 5000,
+    company : "Microsoft",
+    image : "assets/images/bill.jpg"
+  }
+
 }
